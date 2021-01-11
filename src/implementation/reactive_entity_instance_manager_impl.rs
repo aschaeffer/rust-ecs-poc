0106441164329109209d8638e092ef7b7b4906d2
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-11 17:16:24 +0100

    This is not yet ready

--- src/implementation/reactive_entity_instance_manager_impl.rs
+++ src/implementation/reactive_entity_instance_manager_impl.rs
@@ -1,51 +1,73 @@
-use crate::api::{
-    EntityTypeManager,
-    ComponentManager,
-    GraphDatabase,
-    ReactiveEntityInstanceManager,
-    ReactivePropertyInstanceManager
-};
-use async_trait::async_trait;
-use waiter_di::*;
-use std::sync::RwLock;
-use uuid::Uuid;
-use std::collections::HashMap;
-use serde_json::Value;
-use std::collections::hash_map::RandomState;
-
-#[wrapper]
-pub struct ReactiveEntityInstances(RwLock<std::collections::HashMap<Uuid, crate::model::ReactiveEntityInstance<'_>>>);
-
-#[provides]
-fn create_external_type_dependency() -> ReactiveEntityInstances {
-    ReactiveEntityInstances(RwLock::new(std::collections::HashMap::new()))
-}
-
-#[component]
-pub struct EntityInstanceManagerImpl {
-
-    graph_database: Wrc<dyn GraphDatabase>,
-
-    component_manager: Wrc<dyn ComponentManager>,
-
-    entity_type_manager: Wrc<dyn EntityTypeManager>,
-
-    property_instance_manager: Wrc<dny ReactivePropertyInstanceManager>,
-
-    entity_instances: ReactiveEntityInstances,
-
-}
-
-#[async_trait]
-#[provides]
-impl ReactiveEntityInstanceManager for EntityInstanceManagerImpl {
-
-    fn create(&self, type_name: String, properties: HashMap<String, Value>) {
-        unimplemented!()
-    }
-
-    fn create_vertex(&self, type_name: String, properties: HashMap<String, Value, RandomState>) {
-        unimplemented!()
-    }
-
-}
+// use crate::api::{
+//     EntityTypeManager,
+//     ComponentManager,
+//     GraphDatabase,
+//     ReactiveEntityInstanceManager,
+//     // ReactivePropertyInstanceManager,
+//     EntityInstanceManager
+// };
+// use crate::model::{
+//     EntityInstance
+// };
+// use async_trait::async_trait;
+// use waiter_di::*;
+// use std::sync::RwLock;
+// use uuid::Uuid;
+// use std::collections::HashMap;
+// use serde_json::Value;
+// use std::collections::hash_map::RandomState;
+//
+// #[wrapper]
+// pub struct ReactiveEntityInstances<'b>(RwLock<std::collections::HashMap<Uuid, crate::model::ReactiveEntityInstance<'b>>>);
+//
+// #[provides]
+// fn create_external_type_dependency<'b>() -> ReactiveEntityInstances<'b> {
+//     ReactiveEntityInstances(RwLock::new(std::collections::HashMap::new()))
+// }
+//
+// #[component]
+// pub struct ReactiveEntityInstanceManagerImpl<'b> {
+//
+//     graph_database: Wrc<dyn GraphDatabase>,
+//
+//     component_manager: Wrc<dyn ComponentManager>,
+//
+//     entity_type_manager: Wrc<dyn EntityTypeManager>,
+//
+//     entity_instance_manager: Wrc<dyn EntityInstanceManager>,
+//
+//     // property_instance_manager: Wrc<dny ReactivePropertyInstanceManager>,
+//
+//     // They live here
+//     entity_instances: ReactiveEntityInstances<'b>,
+//
+// }
+//
+// #[async_trait]
+// #[provides]
+// impl<'b> ReactiveEntityInstanceManager for ReactiveEntityInstanceManagerImpl<'b> {
+//
+//     fn has(&self, id: Uuid) -> bool {
+//         unimplemented!()
+//     }
+//
+//     fn get(&self, name: String) -> Option<EntityInstance> {
+//         unimplemented!()
+//     }
+//
+//     fn create(&self, type_name: String, properties: HashMap<String, Value, RandomState>) {
+//         unimplemented!()
+//     }
+//
+//     fn delete(&self, id: Uuid) {
+//         unimplemented!()
+//     }
+//
+//     fn import(&self, path: String) {
+//         unimplemented!()
+//     }
+//
+//     fn export(&self, name: String, path: String) {
+//         unimplemented!()
+//     }
+// }
