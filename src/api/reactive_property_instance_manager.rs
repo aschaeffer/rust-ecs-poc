d6d201ae4247e091dbf8aab450ffb7e871f391ba
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-11 17:16:03 +0100

    Added TODO for automatically disconnecting reactive property instances

--- src/api/reactive_property_instance_manager.rs
+++ src/api/reactive_property_instance_manager.rs
@@ -6,6 +6,7 @@ pub trait ReactivePropertyInstanceManager: Send + Sync {
 
     fn connect(&self, outbound_uuid: Uuid, inbound_uuid: Uuid);
 
+    // TODO: automatically disconnect if ReactivePropertyInstance is destroyed
     fn disconnect(&self, outbound_uuid: Uuid, inbound_uuid: Uuid);
 
 }
