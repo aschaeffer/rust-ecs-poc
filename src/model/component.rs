8a2291132cd892e4207dbee4411f6acb831342ac
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-12 18:47:15 +0100

    Added code documentation

--- src/model/component.rs
+++ src/model/component.rs
@@ -3,15 +3,23 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct Component {
+
+    /// The name of the component.
     pub name: String,
+
+    /// The properties which are applied on entity instances.
     pub properties: Vec<PropertyType>,
+
 }
 
 impl Component {
+
+    /// Constructs a new component with the given name and properties
     pub fn new (name: String, properties: Vec<PropertyType>) -> Component {
         Component {
             name,
             properties,
         }
     }
+
 }
