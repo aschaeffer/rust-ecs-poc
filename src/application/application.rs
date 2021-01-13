1be23d0dba2026a57261fcdf6fd09ebbf42e27b5
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-13 19:04:43 +0100

    Removed test code (these will be migrated to unit tests) and added a minimal lifecycle

--- src/application/application.rs
+++ src/application/application.rs
@@ -1,91 +1,70 @@
 use crate::api::*;
 use waiter_di::*;
 use async_trait::async_trait;
+use std::sync::RwLock;
+use std::thread;
+use std::time::Duration;
+
+#[wrapper]
+pub struct RunningState(RwLock<bool>);
+
+#[provides]
+fn create_external_type_dependency() -> RunningState {
+    RunningState(RwLock::new(false))
+}
 
 #[async_trait]
-pub trait Application {
-    // fn new() -> Self where Self: Sized;
-    async fn init(&mut self);
+pub trait Application : Send + Sync {
+
+    fn init(&self);
+
     async fn run(&mut self);
+
+    fn stop(&self);
+
 }
 
 #[module]
 pub struct ApplicationImpl {
-    component_manager: Wrc<dyn ComponentManager>,
-    entity_type_manager: Wrc<dyn EntityTypeManager>,
-    entity_instance_manager: Wrc<dyn EntityInstanceManager>,
-    system_constants_initializer: Wrc<dyn SystemConstantsInitializer>,
-    graph_database: Wrc<dyn GraphDatabase>,
+
+    running: RunningState,
+
+    pub component_manager: Wrc<dyn ComponentManager>,
+    pub entity_type_manager: Wrc<dyn EntityTypeManager>,
+    pub entity_instance_vertex_manager: Wrc<dyn EntityInstanceVertexManager>,
+    pub entity_instance_manager: Wrc<dyn EntityInstanceManager>,
+    pub system_constants_initializer: Wrc<dyn SystemConstantsInitializer>,
+    pub graph_database: Wrc<dyn GraphDatabase>,
+
 }
 
 #[async_trait]
 #[provides]
 impl Application for ApplicationImpl {
 
-    async fn init(&mut self) {
-        println!("--- INIT ---");
+    fn init(&self) {
         self.component_manager.load_static_components();
-        self.component_manager.list_components();
         self.entity_type_manager.load_static_entity_types();
-        self.entity_type_manager.list_entity_types();
         self.system_constants_initializer.activate();
     }
 
     async fn run(&mut self) {
-        println!("--- entity_type_manager.register ---");
-        self.entity_type_manager.register(
-            crate::model::EntityType::new(
-                String::from("test1"),
-                vec![
-                    String::from("positionable")
-                ],
-                vec![
-                    crate::model::PropertyType::new(String::from("x"), String::from("string"))
-                ]
-            )
-        );
-        println!("--- entity_type_manager.create ---");
-        self.entity_type_manager.create(
-            String::from("test2"),
-            vec![
-                String::from("positionable"),
-                String::from("movable")
-            ],
-            vec![
+        {
+            let mut running = self.running.0.write().unwrap();
+            *running = true;
+        }
+        let running = self.running.0.read().unwrap();
+        while *running {
+            thread::sleep(Duration::from_millis(1));
+        }
 
-            ]
-        );
-        println!("--- entity_type_manager.list_entity_types ---");
-        self.entity_type_manager.list_entity_types();
-        println!("--- component_manager.list_components ---");
-        self.component_manager.list_components();
-        println!("--- component_manager.create ---");
-        self.component_manager.create(
-            String::from("dynamically_created"),
-            vec![
-                crate::model::PropertyType::new(String::from("x"), String::from("string")),
-            ]
-        );
-        println!("--- component_manager.list_components ---");
-        self.component_manager.list_components();
-        println!("--- entity_type_manager.list_entity_types ---");
-        self.entity_type_manager.list_entity_types();
-        println!("--- entity_type_manager.delete ---");
-        self.entity_type_manager.delete(String::from("camera"));
-        println!("--- component_manager.list_components ---");
-        self.component_manager.list_components();
-        println!("--- list_components.delete ---");
-        self.component_manager.delete(String::from("dynamically_created"));
-        println!("--- component_manager.list_components ---");
-        self.component_manager.list_components();
-        println!("--- component_manager.get -> JSON ---");
-        let component = self.component_manager.get(String::from("movable")).unwrap();
-        println!("{}", serde_json::to_string(&component).unwrap());
-        self.component_manager.export(String::from("movable"), String::from("C:\\movable.json"));
-        self.component_manager.export(String::from("movable"), String::from("movable.json"));
-        self.component_manager.list_components();
-        self.component_manager.import(String::from("imported_component.json"));
-        self.component_manager.list_components();
+    }
+
+    fn stop(&self) {
+        {
+            let mut running = self.running.0.write().unwrap();
+            *running = false;
+        }
     }
 
 }
