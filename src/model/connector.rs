592565671c7643f31bd177d4be411ab2a0227b8b
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-13 21:19:09 +0100

    rustfmt

--- src/model/connector.rs
+++ src/model/connector.rs
@@ -3,26 +3,25 @@ use bidule::Stream;
 use serde_json::Value;
 
 pub struct Connector<'a, 'b, 'c> {
-
     // Connector uuid
     // pub id: Uuid,
-
     pub inbound: ReactivePropertyInstance<'a>,
 
     pub connection: Stream<'b, Value>,
 
     pub outbound: ReactivePropertyInstance<'c>,
-
 }
 
 impl Connector<'_, '_, '_> {
-
-    fn new(inbound: ReactivePropertyInstance<'static>, outbound: ReactivePropertyInstance<'static>) -> Connector<'static, 'static, 'static> {
+    fn new(
+        inbound: ReactivePropertyInstance<'static>,
+        outbound: ReactivePropertyInstance<'static>,
+    ) -> Connector<'static, 'static, 'static> {
         let connection = Stream::new();
         let connector = Connector {
             connection,
             inbound,
-            outbound
+            outbound,
         };
         //
         // outbound.stream.read().unwrap().observe(move |value| {
@@ -33,5 +32,4 @@ impl Connector<'_, '_, '_> {
         // connector.inbound.stream.write().unwrap().send(value);
         // TODO: connector
     }
-
 }
