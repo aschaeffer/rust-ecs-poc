a295a5dd90b0c508d59c9df60f6f459946ba4bc9
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-13 21:15:55 +0100

    rustfmt

--- src/api/component_manager.rs
+++ src/api/component_manager.rs
@@ -2,12 +2,11 @@ use crate::model::PropertyType;
 use async_trait::async_trait;
 
 #[async_trait]
-pub trait ComponentManager : Send + Sync {
-
-    fn register (&self, component: crate::model::Component);
-    fn load_static_components (&self);
-    fn get_components (&self) -> Vec<crate::model::Component>;
-    fn list_components (&self);
+pub trait ComponentManager: Send + Sync {
+    fn register(&self, component: crate::model::Component);
+    fn load_static_components(&self);
+    fn get_components(&self) -> Vec<crate::model::Component>;
+    fn list_components(&self);
 
     fn has(&self, name: String) -> bool;
     fn get(&self, name: String) -> Option<crate::model::Component>;
@@ -17,5 +16,4 @@ pub trait ComponentManager : Send + Sync {
 
     fn import(&self, path: String);
     fn export(&self, name: String, path: String);
-
 }
