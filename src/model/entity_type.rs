c2cd39e15b1f1220dab1db18e350c41416e00690
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-13 19:01:45 +0100

    Added method which returns true, if the entity type has a property with a given name

--- src/model/entity_type.rs
+++ src/model/entity_type.rs
@@ -35,6 +35,13 @@ impl EntityType {
         self.components.contains(&component_name)
     }
 
+    /// Returns true, if the entity type contains a property with the given name.
+    pub fn has_property(&self, property_name: String) -> bool {
+        !self.properties.iter()
+            .filter(|&p| p.name == property_name)
+            .collect::<Vec<_>>()
+            .is_empty()
+    }
 }
 
 fn empty_string() -> String {
