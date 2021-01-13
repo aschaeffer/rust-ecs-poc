a7e7ea427356c2e4406edd16e8f402507a66007a
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-13 19:06:38 +0100

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use crate::application::Application;
-// use crate::api::*;
 use crate::di::*;
 use waiter_di::*;
 
@@ -16,11 +15,6 @@ async fn main () {
     let mut container = di_container::get::<profiles::Default>();
     let container = &mut container;
     let mut application = Provider::<dyn Application>::create(container);
-    // let component_manager= Provider::<dyn ComponentManager>::get_ref(container);
-    // let entity_type_manager= Provider::<dyn EntityTypeManager>::get_ref(container);
-    // let mut application = Provider::<dyn Application>::create(&mut container);
-    // Provider::<dyn ComponentManager>::get_ref(&mut container);
-    // Provider::<dyn ComponentManager>::get_ref(&mut container);
 
     // component_manager.load_static_components();
     application.init().await;
