f93e7db6fd05eb3e885842cc92cc1e9a398ef215
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-13 21:21:18 +0100

    rustfmt

--- src/api/entity_instance_vertex_manager.rs
+++ src/api/entity_instance_vertex_manager.rs
@@ -1,15 +1,14 @@
 use async_trait::async_trait;
+use indradb::{Vertex, VertexProperties};
 use serde_json::Value;
 use std::collections::HashMap;
 use uuid::Uuid;
-use indradb::{Vertex, VertexProperties};
 
 #[derive(Debug)]
 pub struct EntityInstanceVertexCreationError;
 
 #[async_trait]
 pub trait EntityInstanceVertexManager: Send + Sync {
-
     /// Returns true, if an entity instance vertex exists with the given UUID.
     fn has(&self, id: Uuid) -> bool;
 
@@ -21,13 +20,21 @@ pub trait EntityInstanceVertexManager: Send + Sync {
     fn get_properties(&self, id: Uuid) -> Option<VertexProperties>;
 
     /// Creates a new vertex with the given type and the given properties.
-    fn create(&self, type_name: String, properties: HashMap<String, Value>) -> Result<Uuid, EntityInstanceVertexCreationError>;
+    fn create(
+        &self,
+        type_name: String,
+        properties: HashMap<String, Value>,
+    ) -> Result<Uuid, EntityInstanceVertexCreationError>;
 
     /// Creates a new vertex with the given id, the given type and the given properties.
     /// This is useful for importing an entity instance with the fixed id.
-    fn create_with_id(&self, type_name: String, id: Uuid, properties: HashMap<String, Value>) -> Result<Uuid, EntityInstanceVertexCreationError>;
+    fn create_with_id(
+        &self,
+        type_name: String,
+        id: Uuid,
+        properties: HashMap<String, Value>,
+    ) -> Result<Uuid, EntityInstanceVertexCreationError>;
 
     /// Deletes the vertex with the given id.
     fn delete(&self, id: Uuid);
-
 }
