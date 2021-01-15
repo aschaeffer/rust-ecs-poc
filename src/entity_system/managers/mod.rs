023b7b404dcbfbbfe697cc9b1603bc98d0c19149
Andreas Schaeffer <andreas@schaeffernet.de>
2021-01-15 23:13:30 +0100

    Cleanup

--- src/entity_system/managers/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod api;
-pub mod implementation;
-
-pub mod graph_database;
-// pub mod component_manager;
-// pub mod entity_type_manager;
-// pub mod relation_type_manager;
-// pub mod entity_instance_manager;
-// pub mod relation_instance_manager;
